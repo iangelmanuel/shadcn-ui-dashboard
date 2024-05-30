6b4a01d38feb5b637169b47feb996badbe93848f
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-29 22:46:15 -0500

    chore: Add @radix-ui/react-accordion dependency and implement accordion component in dashboard page

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,41 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+
+const items = [
+  {
+    id: 'item-1',
+    question: 'Is it accessible?',
+    answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-2',
+    question: 'Is it styled?',
+    answer:
+      'Yes. It comes with default styles that matches the other components aesthetic.',
+  },
+  {
+    id: 'item-3',
+    question: 'Is it animated?',
+    answer:
+      "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+]
+
 export default function AccordionPage() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="single" collapsible className="w-full">
+        {items.map((item) => (
+          <AccordionItem key={item.id} value={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   )
 }
