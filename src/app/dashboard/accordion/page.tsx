f8870806eaf76149374e377d7aff9e16232f2216
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-29 22:11:01 -0500

    chore: Initializing configuration project to work with shadcn

---
+++ src/app/dashboard/accordion/page.tsx
@@ -0,0 +1,7 @@
+export default function AccordionPage() {
+  return (
+    <div>
+      <h1>Accordion Page</h1>
+    </div>
+  )
+}
---
+++ src/app/dashboard/alert/page.tsx
@@ -0,0 +1,7 @@
+export default function AlertPage() {
+  return (
+    <div>
+      <h1>Alert Page</h1>
+    </div>
+  )
+}
