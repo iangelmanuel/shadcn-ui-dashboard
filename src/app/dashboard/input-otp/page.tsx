2b6f6307797c1618c105881b678eaaa5b56eff87
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-09 03:07:48 -0500

    feat: Add input OTP component to dashboard layout

---
+++ src/app/dashboard/input-otp/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import * as React from 'react'
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from '@/components/ui/input-otp'
+
+export default function InputOTPPage() {
+  const [value, setValue] = React.useState('')
+  return (
+    <div className="flex flex-col justify-center items-center h-[250px]">
+      <InputOTP
+        maxLength={6}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      <div className="block">{value}</div>
+    </div>
+  )
+}
