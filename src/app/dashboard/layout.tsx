7505286d778a33e7f5230303ee6415b162aa2240
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-10 03:48:21 -0500

    feat: Add sheet component to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,6 +19,7 @@ const links = [
   { name: 'menu bar', href: 'menu-bar' },
   { name: 'input OTP', href: 'input-otp' },
   { name: 'progress', href: 'progress' },
+  { name: 'sheet', href: 'sheet' },
 ].sort((a, b) => a.name.localeCompare(b.name))
 
 export default function DashboardLayout({
